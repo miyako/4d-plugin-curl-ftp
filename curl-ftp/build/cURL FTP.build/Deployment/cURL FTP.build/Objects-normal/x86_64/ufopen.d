6639aaf5955970db8e4380c45e3607023f4b81ea
Keisuke Miyako <miyako@wakanda.jp>
2018-06-27 13:54:39 +0900

    remove obj

--- curl-ftp/build/cURL FTP.build/Deployment/cURL FTP.build/Objects-normal/x86_64/ufopen.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/miyako/Documents/GitHub/4d-plugin/cross-platform/4d-plugin-curl-ftp/curl-ftp/ufopen.c \
-  /Users/miyako/Documents/GitHub/4d-plugin/cross-platform/4d-plugin-curl-ftp/curl-ftp/ufopen.h
