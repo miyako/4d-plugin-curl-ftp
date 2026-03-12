be1d3a484e7367eb282c319745c3d70e60bdaae8
Keisuke Miyako <keisuke.miyako@4D.com>
2026-03-12 13:42:18 +0900

    Update 4DPlugin-cURL-FTP.cpp

--- ftp/4DPlugin-cURL-FTP.cpp
+++ ftp/4DPlugin-cURL-FTP.cpp
@@ -1080,8 +1080,22 @@ void curl_get_info(CURL *curl, CUTF16String& json) {
         info["contentLengthUpload"] = (Json::Int64)contentLengthUploadT;
     
     if(CURLE_OK == curl_easy_getinfo(curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD_T, &contentLengthDownloadT))
+    {
+        // CHANGED: Ensure consistency for 0-byte files queried via SFTP
+        if(contentLengthDownloadT == -1)
+        {
+            char *url = NULL;
+            if((CURLE_OK == curl_easy_getinfo(curl, CURLINFO_EFFECTIVE_URL, &url)) && url)
+            {
+                if(strncmp(url, "sftp://", 7) == 0)
+                {
+                    contentLengthDownloadT = 0;
+                }
+            }
+        }
         info["contentLengthDownload"] = (Json::Int64)contentLengthDownloadT;
-    
+    }
+
     if(CURLE_OK == curl_easy_getinfo(curl, CURLINFO_SPEED_UPLOAD_T, &speedUploadT))
         info["speedUpload"] = (Json::UInt64)speedUploadT;
     
